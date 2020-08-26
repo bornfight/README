76f286370559467e340b8f753142c3187eebe2dc
renatoruk <renato.ruk@gmail.com>
2020-08-26 17:26:43 +0200

    add CODE_OF_CONDUCT.md to danger.js skip array

--- dangerfile.ts
+++ dangerfile.ts
@@ -1,10 +1,10 @@
-import { danger, warn, fail } from "danger"
-import { readFileSync } from "fs"
-import { basename } from "path"
+import { danger, warn, fail } from "danger";
+import { readFileSync } from "fs";
+import { basename } from "path";
 
 const files = [...danger.git.created_files, ...danger.git.modified_files];
 const markdowns = files.filter(f => f.indexOf(".md") !== -1);
-const skipTODOCheck = ["dangerfile.ts", "CONTRIBUTING.md"];
+const skipTODOCheck = ["dangerfile.ts", "CONTRIBUTING.md", "CODE_OF_CONDUCT.md"];
 
 // Check for [TO-DO]s in all files changed
 
@@ -24,7 +24,7 @@ markdowns.forEach(f => {
     if (skipTODOCheck.indexOf(f) !== -1) {
         return;
     }
-    
+
     const content = readFileSync(f, "utf8");
     const lines = content.split("\n");
     lines.forEach(l => {
@@ -32,14 +32,14 @@ markdowns.forEach(f => {
             if (lines.indexOf(word.word) !== -1) {
                 const isLocal = !danger.github;
                 const message = isLocal ? `${word.reason} in ${f}:${lines.indexOf(l) + 1}` : word.reason;
-                warn(message, f, lines.indexOf(l) + 1)
+                warn(message, f, lines.indexOf(l) + 1);
             }
-        })
-    })
+        });
+    });
 });
 
 // Ensure we stay with kebab-case
 const underscores = danger.git.created_files.filter(f => basename(f).indexOf("_") !== -1);
 underscores.forEach(path => {
-    fail(`Found a new file with underscores, this repo uses kebab-case for files: ${path}`)
+    fail(`Found a new file with underscores, this repo uses kebab-case for files: ${path}`);
 });
